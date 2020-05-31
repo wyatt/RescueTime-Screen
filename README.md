51e3ee7080ea4dade6ff3884ddbca9fd24e22283
profwyattb <profwyattb@gmail.com>
2020-05-31 20:28:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# RescueTime-Screen
+A Flask app that queries RescueTime data and calculates daily screen time to create a screen with a running total. It queries RescueTime every minute.
+
+## Screenshots
+Light Mode             |  Dark Mode            | Example usage
+:---------------------:|:---------------------:|:-------------------------:
+![](images/light.png)  |  ![](images/dark.png) | ![](images/example_usage.jpg)
+## Installation
+1. [Clone](https://github.com/profwyattb/RescueTime-Screen/archive/master.zip) this repository
+1. Go to [RescueTime](https://www.rescuetime.com/anapi/manage) and create a new key
+1. Edit app.py and paste the key where indicated
+1. Run `pip3 install -r requirements.txt`
+## Usage
+1. To start the flask server, run `python3 app.py`
---
+++ app.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Importing libraries
+from flask import Flask, request, render_template
+import requests
+import json
+import html2text
+import time
+
+# Flask config
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+# Insert your key from RescueTime here
+key = "<YOUR_KEY>"
+
+@app.route('/', methods=['POST', 'GET'])
+def display():
+    # Get RescueTime data
+    try:
+        jsondata = requests.get("https://www.rescuetime.com/anapi/data?key=" + key).text
+    except requests.ConnectionError:
+        time.sleep(5)
+        display()
+    # Parse RecueTime data
+    h = html2text.HTML2Text()
+    jsondata = h.handle(jsondata)
+    jsondata = jsondata.replace('\n', '')
+    try:
+        jsondata = json.loads(jsondata)
+    except json.JSONDecodeError:
+        time.sleep(5)
+        display()
+    # Calculates and formats total time
+    total = 0
+    for x in range(0, len(jsondata["rows"])):
+        total += jsondata["rows"][x][1]
+    hours = int(total/3600)
+    mins = int((((total/3600)*60)%60))
+    if mins < 10:
+        mins = str("0" + str(mins))
+    totaltime = str(str(hours) + "<span>:</span>" + str(mins))
+    # Inserts data into time.html
+    return render_template("time.html", total = totaltime)
+
+# Runs app in debug mode
+if __name__ == '__main__':	
+    app.run(debug=True)
